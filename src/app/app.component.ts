a95c0eb2fcd1bbc79d735ac925cb44d995ec12c6
Leonardod <lfdantoni@gmail.com>
2020-01-01 20:02:16 -0300

    added basic logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserManagerService, UserState, User } from './user-manager.service';
+import { takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-decorator';
+export class AppComponent implements OnInit, OnDestroy {
+
+  userName = '';
+
+  private alive = true;
+
+  constructor(private manager: UserManagerService) {
+
+  }
+
+  ngOnInit(): void {
+   this.manager.stateChange
+    .pipe(takeWhile(() => this.alive))
+    .subscribe(({stateType, state}) => this.processState(stateType, state))
+  }
+
+  ngOnDestroy(): void {
+    this.alive = false;
+  }
+
+  getUserClick() {
+    this.manager.getUser()
+  }
+
+  private processState(stateType: UserState, state: User) {
+    switch (stateType) {
+      case UserState.GET_USER_SUCCESS:
+        this.userName = state.name;
+        break;
+      case UserState.GET_USER:
+        this.userName = 'loading...';
+        break;
+    }
+
+    console.group('processState');
+    console.log(stateType)
+    console.log(state)
+    console.groupEnd()
+  }
 }
---
+++ src/app/user-manager.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+export enum UserState {
+  INIT_STATE,
+  GET_USER,
+  GET_USER_SUCCESS,
+  GET_USER_FAIL
+}
+
+export class User {
+  name: string;
+  email: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserManagerService {
+  stateChange = new BehaviorSubject<{stateType: UserState, state: User}>({
+    stateType: UserState.INIT_STATE,
+    state: this.getInitState()
+  })
+
+  constructor() { }
+
+  getUser() {
+    this.updateState(UserState.GET_USER);
+
+    setTimeout(
+      () => this.updateState(UserState.GET_USER_SUCCESS, {email: 'test@test.com', name: 'test'}) ,
+      5000);
+  }
+
+  getState(): User {
+    return {...this.stateChange.getValue().state}
+  }
+
+  private updateState(stateType: UserState, state: User = this.getState()) {
+    this.stateChange.next({stateType, state});
+  }
+
+  private getInitState(): User {
+    return {
+      email: '',
+      name: ''
+    }
+  }
+}
